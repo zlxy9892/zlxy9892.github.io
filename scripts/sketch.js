20c6f62bb386f434622fcadd0ad5db39a2779965
Frank <Frank>
2017-02-27 18:47:36 +0800

    Add matrix rain.

---
+++ scripts/sketch.js
@@ -0,0 +1,81 @@
+var symbolSize = 24;
+var streams = [];
+
+function setup() {
+  createCanvas(
+  	window.innerWidth,
+  	window.innerHeight
+  );
+  background(0);
+  
+  var x = 0;
+  for (var i = 0; i <= width / symbolSize; i++) {
+  	stream = new Stream();
+  	stream.generateSymbols(x, random(-1000, 0));
+  	streams.push(stream);
+  	x += symbolSize;
+  }
+
+  textSize(symbolSize);
+}
+
+function draw() {
+	background(0, 150);
+	streams.forEach(function(stream) {
+		stream.render();
+	});
+}
+
+function Symbol(x, y, speed, first) {
+	this.x = x;
+	this.y = y;
+	this.value;
+	this.speed = speed;
+	this.switchInterval = round(random(2, 20));
+	this.first = round(random(0, 4)) == 1;
+
+	this.setToRandomSymbol = function() {
+		if (frameCount % this.switchInterval == 0) {
+			// set it to Katakana 日文			
+			this.value = String.fromCharCode(
+				0x30A0 + round(random(0, 96))
+			);
+		}
+	}
+
+	this.rain = function() {
+		this.y = (this.y >= height) ? 0 : this.y += this.speed;
+	}
+
+}
+
+function Stream() {
+	this.symbols = [];
+	this.totalSymbols = round(random(5, 30));
+	this.speed = random(5, 20);
+
+	this.generateSymbols = function(x, y) {
+		var first = true;
+		for (var i = 0; i <= this.totalSymbols; i++) {
+			symbol = new Symbol(x, y, this.speed, first);
+			symbol.setToRandomSymbol();
+			this.symbols.push(symbol);
+			y -= symbolSize;
+			first = false;
+		}
+	}
+
+	this.render = function() {
+		this.symbols.forEach(function(symbol) {
+			if (symbol.first) {
+				fill(180, 255, 180);
+			} else {
+				fill(0, 255, 70);
+			}
+			text(symbol.value, symbol.x, symbol.y);
+			symbol.rain();
+			symbol.setToRandomSymbol();
+		});
+	}
+	
+}
